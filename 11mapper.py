267bc84dc7f611fcc5a876821c035e8d5fc686b4
sushma95 <54374529+sushma95@users.noreply.github.com>
2020-05-26 13:07:58 -0500

    changed Store to department in two files 11mapper.py,13reducer.py.

--- 11mapper.py
+++ 11mapper.py
@@ -13,8 +13,8 @@ with open("part.txt", "r") as input:
         date, time,store, department, amount, paymentType = datalist
 
         # output intermediate key-value pairs
-        output.write(store + "\t" + amount + "\n")
+        output.write(department + "\t" + amount + "\n")
 
         # display to console (not required - just for the user)
-        print(store + "\t" + amount + "\n")
+        print(department + "\t" + amount + "\n")
 
